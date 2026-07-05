2ae018dfa3b8f341c53273f02ae95c732c0e594a
New <suhaendris18@gmail.com>
2026-07-05 18:15:33 +0300

    complete clear all, color picker, style milestones,and add README

---
+++ Readme.md
@@ -0,0 +1,43 @@
+#📝 my To-Do List
+IT IS THE MINI TO-DO LIST
+## Table of contents
+
+- Overview
+  - The challenge
+  - Screenshot
+
+- My process my-process
+  - Built with built-with
+  - What I learned
+
+- Author
+
+## Overview
+
+### The challenge
+Users should be able to:
+- Add, toggle completion , and delete tasks.
+- View a live counter of remaining tasks.
+- Wipe the entire list using a "Clear All" button.
+- Switch between 7 background themes using a color picker circles interface.i mean the bonus 1
+
+### Screenshot
+*(![alt text](image.png))*
+
+
+
+
+## My process
+
+### Built with
+- given HTML5 
+-  given CSS
+-  JavaScript nasic concepts
+
+### What I learned
+I learned how to safely add many buttons and also to doing todollist very well
+and also many things in this project
+
+
+## Author
+- suha Endris
--- script.js
+++ script.js
@@ -1,70 +1,150 @@
 let tasks = [];
+document.addEventListener('DOMContentLoaded', function() {
+  const taskInput = document.getElementById('taskInput');
+  const addBtn = document.getElementById('addBtn');
+  const taskList = document.getElementById('taskList');
+  const errorMsg = document.getElementById('errorMsg');
+  const counterText = document.getElementById('counter');
+  const clearBtn = document.getElementById('clearBtn');
+  const allDoneMsg = document.getElementById('allDoneMsg');
+  const colorCircles =document.querySelectorAll(".color-circle");
+  // loadTasks();
+//   let savedTasks = localStorage.getItem("tasks");
 
-const taskInput = document.getElementById('taskInput');
-const addBtn = document.getElementById('addBtn');
-const taskList = document.getElementById('taskList');
-const errorMsg = document.getElementById('errorMsg');
-const counterText = document.getElementById('counter');
-
-function updateCounter() {
-  const undoneTasks = tasks.filter(task => !task.done).length;
-  counterText.innerHTML = 'Tasks remaining: <span id="remainingCount">' + undoneTasks + '</span>';
-}
-
-addBtn.addEventListener('click', function() {
-  const textValue = taskInput.value.trim();
+//   if (savedTasks) {
+//      tasks = JSON.parse(savedTasks);
+// }
+   if (allDoneMsg) {
+         allDoneMsg.style.display = 'none';
+ }
   
-  errorMsg.textContent = '';
 
-  if (textValue === '') {
-    errorMsg.textContent = 'Please type a task first';
-    return;
+  function updateCounter() {
+    const undoneTasks = tasks.filter(task => !task.done).length;
+    let completed = tasks.filter(function(task){
+        return task.done == true;}).length;
+    counterText.innerHTML = 'Tasks remaining: <span id="remainingCount">' + undoneTasks + '</span> <br>'+
+    completed + " of " + tasks.length + " tasks completed";
+    if (tasks.length > 0 && undoneTasks== 0) {
+        counter.textContent = "🎉 All tasks done! Great job!";
+        counter.classList.add("all-done-msg");
+        counter.classList.add("visible");
+    }
+
   }
 
-  const taskObject = { text: textValue, done: false };
-  tasks.push(taskObject);
-
-  const li = document.createElement('li');
-  li.className = 'task-item';
-
-  const textSpan = document.createElement('span');
-  textSpan.className = 'task-text';
-  textSpan.textContent = textValue;
-
-  const doneBtn = document.createElement('button');
-  doneBtn.textContent = 'Done';
-  doneBtn.className = 'done-btn';
-
-  doneBtn.addEventListener('click', function() {
-    taskObject.done = !taskObject.done;
-    if (taskObject.done) {
-      li.className = 'task-item done';
-      doneBtn.textContent = 'Undone';
-    } else {
-      li.className = 'task-item';
-      doneBtn.textContent = 'Done';
+  addBtn.addEventListener('click', function() {
+    const textValue = taskInput.value.trim();
+    errorMsg.textContent = '';
+    if (textValue === '') {
+      errorMsg.textContent = 'Please type a task first';
+      return;
     }
+  let taskNames=[];
+    
+  for (let i = 0; i < tasks.length; i++) {
+        taskNames.push(tasks[i].text.toLowerCase());
+    }
+
+    if (taskNames.includes(textValue .toLowerCase())) {
+        errorMsg.textContent = "This task already exists!";
+        return;
+    }
+
+
+    const taskObject = { text: textValue, done: false };
+    tasks.push(taskObject);
+    // saveTasks();
+    // localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    const li = document.createElement('li');
+    li.className = 'task-item';
+    //localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    const textSpan = document.createElement('span');
+    textSpan.className = 'task-text';
+    textSpan.textContent = textValue;
+
+    const doneBtn = document.createElement('button');
+    doneBtn.textContent = 'Done';
+    doneBtn.className = 'done-btn';
+
+    doneBtn.addEventListener('click', function() {
+      taskObject.done = !taskObject.done;
+      if (taskObject.done) {
+        li.classList.add('done');
+        doneBtn.textContent = 'Undone';
+      } else {
+        li.classList.remove('done');
+        doneBtn.textContent = 'Done';
+      }
+      updateCounter();
+    });
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.className = 'delete-btn';
+
+    deleteBtn.addEventListener('click', function() {
+      const index = tasks.indexOf(taskObject);
+      if (index > -1) {
+        tasks.splice(index, 1);
+      }
+      li.remove();
+      updateCounter();
+    });
+
+    li.appendChild(textSpan);
+    li.appendChild(doneBtn);
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+
+    taskInput.value = '';
     updateCounter();
   });
 
-  const deleteBtn = document.createElement('button');
-  deleteBtn.textContent = 'Delete';
-  deleteBtn.className = 'delete-btn';
 
-  deleteBtn.addEventListener('click', function() {
-    const index = tasks.indexOf(taskObject);
-    if (index > -1) {
-      tasks.splice(index, 1);
-    }
-    li.remove();
-    updateCounter();
+  if (clearBtn) {
+    clearBtn.addEventListener('click', function() {
+      if (confirm('Are you sure you want to delete all tasks?')) {
+        tasks = []; 
+        taskList.innerHTML = ''; 
+        updateCounter(); 
+      }
+    });
+  }
+  colorCircles.forEach(circle => {
+    circle.addEventListener('click', function(){
+      colorCircles.forEach(c => c.classList.remove('active'));
+      circle.classList.add('active');
+      document.body.style.backgroundColor = circle.dataset.color;
+    });
   });
 
-  li.appendChild(textSpan);
-  li.appendChild(doneBtn);
-  li.appendChild(deleteBtn);
-  taskList.appendChild(li);
-
-  taskInput.value = '';
   updateCounter();
+
+// function saveTasks(){
+//     let tasks = [];
+//     taskList.querySelectorAll('li').forEach(function(item){
+//         const textSpan = item.querySelector('.task-text');
+        
+//         if (textSpan) {
+//             tasks.push({
+//                 text:textSpan.textContent.trim(),
+//                 done:item.classList.contains('done')
+//             })
+            
+//         }
+    
+
+//     });
+//     localStorage.setItem('tasks',JSON.stringify(tasks));
+//  }
+// function loadTasks(){
+//     taskList.innerHTML ='';
+//     const tasks= JSON.parse(localStorage.getItem('tasks')) || [];
+//     tasks.forEach(createTaskElement);
+//     }
+
+
 });
